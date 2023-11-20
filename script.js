7bec71c1352fac590d780f53008006c1aff2d09f
RomulloOliveira <felipe2006__@hotmail.com>
2023-11-20 12:14:39 -0300

    Desafio valor do input

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const input = document.querySelector("#main-input")
+
+
+function digiteiNoInput(){
+    console.log(input.value);
+}
+
+function colocarNaTela(){
+    const p = document.querySelector(".resultado")
+    p.innerHTML = input.value
+    console.log
+    
+}
